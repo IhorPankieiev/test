18dbffff51242935d545275b20e6b562a330d15e
Ihor Pankieiev <igorpankeyev@gmail.com>
2022-10-20 22:36:10 +0300

    Arrays was added

---
+++ Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myArr = {1, 22, 34, 23, 66, 15, 6, 12, 10, 9};
+        for (int i = (myArr.length - 1); i >= 0; i--){
+            System.out.print(" " + myArr[i]);
+        }
+    }
+}
